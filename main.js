2deb484de2c150fcb726c925c6e6861dbf3c792d
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-11 00:19:58 -0500

    finish

---
+++ main.js
@@ -0,0 +1,39 @@
+const head = document.querySelector('h1')
+head.style.fontSize = '300px'
+head.style.color = 'red';
+head.style.wordSpacing = '-800px'
+
+
+const body = document.querySelector('body')
+body.style.backgroundImage = "url('https://miro.medium.com/max/512/1*u76EeWCYWaEFZ37_2YY4DA.jpeg')"
+const paragraph = document.querySelector('p')
+paragraph.style.color = 'grey'
+paragraph.style.outline = 'thick dotted yellow'
+paragraph.style.outlineWidth = '30px'
+paragraph.style.wordSpacing = '-10px'
+const list = document.querySelector('ul')
+list.style.color = 'blue';
+body.style.color = 'red'
+
+list.style.backgroundColor = 'black'
+const ol = document.querySelector('ol')
+ol.style.border = 'thick solid green'
+list.style.textAlign = 'right'
+ol.style.transform = 'rotate(60deg)'
+// ol.style.webkitTextStroke
+const h3 = document.querySelector('h3')
+h3.style.unicodeBidi = 'embed'
+h3.style.fontSize = '35px'
+h3.style.color = 'lightGreen'
+h3.style.textShadow = "5px 5px 1px #ff0000, 10px 10px 1px #0000ff"
+// h3.style.animation = 'myNewMove 4s 2'
+// h3.style.animationName = 'myNewMove'
+
+const form = document.querySelector('form')
+form.style.backgroundColor = 'purple'
+form.style.overflow = 'scroll'
+form.style.transform = 'rotate(180deg)'
+// form.style.position = 'sticky'
+
+const textArea = document.querySelector('textarea')
+textArea.style.backgroundColor = 'orange'
